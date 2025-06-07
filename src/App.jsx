5e4243b1dc5c6b15688bd868479481518e8c3d2c
SashaGonchar <nekospoon228@gmail.com>
2025-06-07 23:45:43 +0300

    git ignore added

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,24 @@
 import {createBrowserRouter, RouterProvider} from "react-router-dom"
 
+import Layout from "./components/Layout.jsx";
 import Home from "./pages/Home.jsx"
 import About from "./pages/About.jsx"
 import Cart from "./pages/Cart.jsx"
 import Categories from "./pages/Categories.jsx"
 import NotFound from "./pages/NotFound.jsx"
 import ProductDetails from "./pages/ProductDetails.jsx"
-import Header from "./components/Header.jsx"
-import Footer from "./components/Footer.jsx";
 
-const router = createBrowserRouter([
-    {
-        path: "/", element: (
-            <>
-                <Header/>
-                <Home/>
-                <Footer/>
-            </>)
-    },
-    {
-        path: "about", element:
-            <>
-                <Header/>
-                <About/>
-                <Footer/>
-            </>
-    },
-    {
-        path: "cart", element:
-            <>
-                <Header/>
-                <Cart/>
-                <Footer/>
-            </>
-    },
-    {
-        path: "categories", element:
-            <>
-                <Header/>
-                <Categories/>
-                <Footer/>
-            </>
-    },
-    {
-        path: "productDetails", element:
-            <>
-                <Header/>
-                <ProductDetails/>
-                <Footer/>
-            </>
-    },
-    {
-        path: "*", element:
-            <>
-                <Header/>
-                <NotFound/>
-                <Footer/>
-            </>
-    },
-]);
+const router = createBrowserRouter([{
+    path: "/", element: <Layout/>, children: [
+        {path: "", element: <Home/>},
+        {path: "about", element: <About/>},
+        {path: "cart", element: <Cart/>},
+        {path: "categories", element: <Categories/>},
+        {path: "productDetails", element: <ProductDetails/>},
+        {path: "*", element: <NotFound/>},
+    ]
+}]);
+
 
 function App() {
 
